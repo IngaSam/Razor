6645fa4b8d4bad2e014a96deaff954c42b257bf7
IngaSam <kayumovainga2@mail.ru>
2023-08-19 12:38:59 +0300

    Добавьте файлы проекта.

---
+++ Razor/Controller/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Razor.Controller
+{
+    public class HomeController: ControllerBase
+    {
+        public IActionResult Index()
+        {
+            Product myProduct = new Product()
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M,
+            };
+
+            return Ok(myProduct);
+        }
+     }
+}
